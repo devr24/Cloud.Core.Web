577b840745e1055ef0942c37174cf069fe6d8000
Robert McCabe <rmccabe@eshopworld.com>
2020-07-08 12:34:13 +0100

    Updated

--- src/Cloud.Core.Web/Extensions/ServiceCollectionExtensions.cs
+++ src/Cloud.Core.Web/Extensions/ServiceCollectionExtensions.cs
@@ -9,7 +9,7 @@
     using AspNetCore.Builder;
     using AspNetCore.Mvc;
     using AspNetCore.Mvc.Abstractions;
-    using Microsoft.AspNetCore.Mvc.ApiExplorer;
+    using AspNetCore.Mvc.ApiExplorer;
     using OpenApi.Models;
     using Swashbuckle.AspNetCore.SwaggerGen;
     using Swashbuckle.AspNetCore.SwaggerUI;
